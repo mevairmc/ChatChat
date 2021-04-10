6bc4a1a730a959314be4a61d07f4f38a78aef761
Marco Evair <mmacolluncoc@uni.pe>
2021-04-10 17:52:23 -0500

    added connect and disconnect event using sockets, only show message in the console

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
-
+const server = http.createServer(app);
+const io = socketio(server);
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Run when client is active
+io.on('connection', socket => {
+
+  socket.emit('message', 'Welcome to ChatChat!!!')
+
+  //Broadcast wehena  suer connects
+
+  socket.broadcast.emit('message','A user has joined to the chat');
+ 
+  //Run when client disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  });
+});
+
 const PORT = 3000 ||process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
