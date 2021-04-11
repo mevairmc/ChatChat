0328ae720b38195a459349dfefba5eb734341edb
Marco Evair <mmacolluncoc@uni.pe>
2021-04-10 19:42:00 -0500

    added current users to room and specific room

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users')
 
 //get username and Room
 
@@ -9,9 +11,15 @@ const {username, room} = Qs.parse(location.search,{
 
 
 const socket = io();
-
+// Join CHatRoom
 socket.emit('joinRoom', {username, room});
 
+
+//Get room and users
+socket.on('roomUsers',({room, users})=>{
+  outputRoomName(room);
+  outputUsers(users);
+})
 
 //Message server
 socket.on('message', message => {
@@ -39,9 +47,22 @@ chatForm.addEventListener('submit', (e) => {
 function outputMessage(message){
   const div = document.createElement('div');
   div.classList.add('message');
-  div.innerHTML =`<p class="meta">${message.username}<span>${message.time}</span></p>
+  div.innerHTML =`<p class="meta">${message.username}  <span>${message.time}</span></p>
   <p class="text">
     ${message.text}
   </p>`;
   document.querySelector('.chat-messages').appendChild(div);
+}
+
+// Add room name to DOm
+
+function outputRoomName(room){
+  roomName.innerText = room;
+}
+
+//Add users to DOM
+function outputUsers(users){
+  userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join('')}
+  `;
 }
--- server.js
+++ server.js
@@ -29,6 +29,11 @@ io.on('connection', socket => {
       formatMessage(username,`${user.username} has joined to the chat`)
     );
 
+    // Send users and room info
+    io.to(user.room).emit('roomUsers',{
+      room: user.room,
+      users: getRoomUsers(user.room)
+    })
   })
 
   // Listen for chatMessage
@@ -44,6 +49,12 @@ io.on('connection', socket => {
 
     if(user){
       io.to(user.room).emit('message',  formatMessage(user.username,`${user.username} has left the chat`));
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers',{
+        room: user.room,
+        users: getRoomUsers(user.room)
+      })
     }
   });
 });
